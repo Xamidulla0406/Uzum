e62e07401410dbe4a78e684fc1e0659dbd5a3334
Javlife <java434799@gmail.com>
2023-02-08 22:19:06 +0500

    upload image

---
+++ src/main/java/uz/nt/uzumproject/dto/ImageDto.java
@@ -0,0 +1,17 @@
+package uz.nt.uzumproject.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ImageDto {
+    private Integer id;
+    private String name;
+    private String extension;
+}
---
+++ src/main/java/uz/nt/uzumproject/model/Image.java
@@ -0,0 +1,23 @@
+package uz.nt.uzumproject.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Image {
+    @Id
+    @GeneratedValue(generator = "imageIdSeq")
+    @SequenceGenerator(name = "imageIdSeq", sequenceName = "image_id_seq", allocationSize = 1)
+    private Integer id;
+    private String name;
+    private String url;
+    private String extension;
+    @ManyToOne
+    private Product product;
+    private LocalDateTime createdAt;
+}
