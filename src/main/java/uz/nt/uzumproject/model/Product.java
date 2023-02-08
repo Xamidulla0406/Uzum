e82d595657e03e29b3236ff80e5142b2f8754b82
MuzaffarO <muzaffarobidjonov0304@gmail.com>
2023-02-08 21:11:42 +0500

    all good

--- src/main/java/uz/nt/uzumproject/model/Product.java
+++ src/main/java/uz/nt/uzumproject/model/Product.java
@@ -20,6 +20,7 @@ public class Product {
     private Integer price;
     private Integer amount;
     private String description;
+
     private String imageUrl;
     private Boolean isAvailable;
 }
