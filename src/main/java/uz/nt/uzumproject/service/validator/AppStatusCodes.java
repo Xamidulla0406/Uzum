d07fb7dce28d8e32e2c995aaef72a772ca57a20c
Bek <120727408+khudoyorov@users.noreply.github.com>
2023-02-09 21:01:27 +0500

    fix bug

--- src/main/java/uz/nt/uzumproject/service/validator/AppStatusCodes.java
+++ src/main/java/uz/nt/uzumproject/service/validator/AppStatusCodes.java
@@ -5,5 +5,5 @@ public class AppStatusCodes {
     public static final Integer NOT_FOUND_CODE = -1;
     public static final Integer OK_CODE = 0;
     public static final Integer DATABASE_ERROR_CODE = 1;
-    public static final Integer UNEXPECTED_ERROR = 2;
+    public static final Integer UNEXPECTED_ERROR_CODE = 2;
 }
