caa27c208d7a5240a7118249d9fd495f501c1e27
FirdavsXayrullayev <firdavsdev4778@gmail.com>
2023-02-09 21:05:43 +0500

    validator

--- src/main/java/uz/nt/uzumproject/service/validator/ProductValidator.java
+++ src/main/java/uz/nt/uzumproject/service/validator/ProductValidator.java
@@ -23,17 +23,17 @@ public class ProductValidator {
         if(dto.getName() == null){
             errorDtoList.add(new ErrorDto("name", NULL_VALUE));
         } else if (dto.getName().length() == 0) {
-            errorDtoList.add(new ErrorDto("amount", EMPTY_STRING));
+            errorDtoList.add(new ErrorDto("name", EMPTY_STRING));
         }
         if(dto.getDescription() == null){
             errorDtoList.add(new ErrorDto("description", NULL_VALUE));
         } else if (dto.getDescription().length() == 0) {
-            errorDtoList.add(new ErrorDto("amount", EMPTY_STRING));
+            errorDtoList.add(new ErrorDto("description", EMPTY_STRING));
         }
         if(dto.getPrice() == null){
-            errorDtoList.add(new ErrorDto("amount", NULL_VALUE));
+            errorDtoList.add(new ErrorDto("price", NULL_VALUE));
         } else if (dto.getPrice() <= 0) {
-            errorDtoList.add(new ErrorDto("amount", NEGATIVE_VALUE));
+            errorDtoList.add(new ErrorDto("price", NEGATIVE_VALUE));
         }
 
         return errorDtoList;
