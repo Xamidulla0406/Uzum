357bb722762c9fe478e9cbe1b63746fa302cbb51
Javlife <java434799@gmail.com>
2023-02-09 20:26:54 +0500

    error constants

--- src/main/java/uz/nt/uzumproject/service/validator/AppStatusMessages.java
+++ src/main/java/uz/nt/uzumproject/service/validator/AppStatusMessages.java
@@ -7,8 +7,6 @@ public class AppStatusMessages {
     public static final String VALIDATION_ERROR = "Validation error";
     public static final String NOT_FOUND = "Data is not found";
     public static final String UNEXPECTED_ERROR = "Unexpected error is occurred";
-    public static final String PARENT_ID_NOT_FOUND = "The parent id is not fount for given data";
     public static final String DATABASE_ERROR = "Database error";
-    public static final String DUPLICATE_ERROR = "Value is duplicated";
     public static final String OK = "OK";
 }
