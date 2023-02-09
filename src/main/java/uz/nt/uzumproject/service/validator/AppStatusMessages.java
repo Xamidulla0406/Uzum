1fae476ffe3ea3e00c3d06799d02d800c3922109
Bek <120727408+khudoyorov@users.noreply.github.com>
2023-02-09 20:50:20 +0500

    APP codes add

---
+++ src/main/java/uz/nt/uzumproject/service/validator/AppStatusMessages.java
@@ -0,0 +1,12 @@
+package uz.nt.uzumproject.service.validator;
+
+public class AppStatusMessages {
+    public static final String NULL_VALUE = "Value is null";
+    public static final String NEGATIVE_VALUE = "Value isValue must be greater than 0";
+    public static final String EMPTY_STRING = "Value empty";
+    public static final String VALIDATION_ERROR = "Validation error";
+    public static final String NOT_FOUND = "Data is not found";
+    public static final String UNEXPECTED_ERROR = "Unexpected error is occured";
+    public static final String DATABASE_ERROR = "Database error";
+    public static final String OK = "OK";
+}
