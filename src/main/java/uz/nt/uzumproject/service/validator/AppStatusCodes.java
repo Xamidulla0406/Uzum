1837f3021cbc9cfe5ee07d7c15839f2f54ccdecd
Javlife <java434799@gmail.com>
2023-02-09 20:26:54 +0500

    error constants

---
+++ src/main/java/uz/nt/uzumproject/service/validator/AppStatusCodes.java
@@ -0,0 +1,9 @@
+package uz.nt.uzumproject.service.validator;
+
+public class AppStatusCodes {
+    public static final Integer VALIDATION_ERROR_CODE = -2;
+    public static final Integer NOT_FOUND_ERROR_CODE = -1;
+    public static final Integer OK_CODE = 0;
+    public static final Integer DATABASE_ERROR_CODE = 1;
+    public static final Integer UNEXPECTED_ERROR_CODE = 2;
+}
