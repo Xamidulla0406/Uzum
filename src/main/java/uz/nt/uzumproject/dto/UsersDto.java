788681091785eb83e6f5ed044112ad90fc756c6c
Diyorbek <diorakhmadaliev@gmail.com>
2023-02-16 18:56:30 +0500

    Exception Handler

--- src/main/java/uz/nt/uzumproject/dto/UsersDto.java
+++ src/main/java/uz/nt/uzumproject/dto/UsersDto.java
@@ -1,6 +1,8 @@
 package uz.nt.uzumproject.dto;
 
 import lombok.*;
+import uz.nt.uzumproject.service.validator.IsValidPhoneNumber;
+import uz.nt.uzumproject.service.validator.ValidGender;
 
 import java.util.Date;
 import java.util.List;
@@ -11,11 +13,13 @@ import java.util.List;
 @NoArgsConstructor
 public class UsersDto {
     private Integer id;
+    @IsValidPhoneNumber
     private String phone;
     private String firstName;
     private String lastName;
     private String middleName;
     private String email;
+    @ValidGender
     private String gender;
     private String birthDate;
     private List<ProductDto> product;
---
+++ src/main/java/uz/nt/uzumproject/service/validator/PhoneNumberValidation.java
@@ -0,0 +1,17 @@
+package uz.nt.uzumproject.service.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.util.List;
+
+public class PhoneNumberValidation implements ConstraintValidator<IsValidPhoneNumber, String> {
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext constraintValidatorContext) {
+        List<String> codes = List.of("33","88","90","91","93","94","95","97",
+                "98","99","71","50","55");
+        return s.length() == 9 && codes.contains(s.substring(0, 2));
+    }
+
+}
