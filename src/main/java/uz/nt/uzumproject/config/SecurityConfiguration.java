01bce6b40be33116c76b00cff9995b11cc99ecb2
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-02-16 18:10:18 +0500

    Hello

--- src/main/java/uz/nt/uzumproject/config/SecurityConfiguration.java
+++ src/main/java/uz/nt/uzumproject/config/SecurityConfiguration.java
@@ -28,6 +28,7 @@ public class SecurityConfiguration {
                     .withUser("a")
                     .password(passwordEncoder().encode("1"))
                     .roles("User");
+//            fjkladsfjaldskfa
     }
 
    @Bean
