6978c51206270cf2778d152c8f9b35634b6a7b7f
Xamidulla0406 <xamidullauzb@gmail.com>
2023-02-23 21:51:14 +0500

    cors

--- src/main/java/uz/nt/uzumproject/config/AppConfig.java
+++ src/main/java/uz/nt/uzumproject/config/AppConfig.java
@@ -3,8 +3,10 @@ package uz.nt.uzumproject.config;
 import com.google.gson.Gson;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 
 @Configuration
+@EnableRedisRepositories
 public class AppConfig {
 
     @Bean
