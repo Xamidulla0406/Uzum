095f9e821112b7a9abe51fb107ddc3edae463763
Axmadjanovich <muhammadkarim@barqapp.com>
2023-02-28 19:30:01 +0500

    native & named & hql queries

--- src/main/java/uz/nt/uzumproject/config/SecurityConfiguration.java
+++ src/main/java/uz/nt/uzumproject/config/SecurityConfiguration.java
@@ -1,7 +1,6 @@
 package uz.nt.uzumproject.config;
 
 import com.google.gson.Gson;
-import io.jsonwebtoken.SignatureException;
 import org.postgresql.Driver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -24,7 +23,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 import uz.nt.uzumproject.dto.ResponseDto;
 import uz.nt.uzumproject.security.JwtFilter;
 import uz.nt.uzumproject.service.UsersService;
