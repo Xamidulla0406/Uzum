3d18479285f57c59219902968ac4de869c9f7c65
Xamidulla0406 <xamidullauzb@gmail.com>
2023-03-01 13:51:33 +0500

    schedule

--- src/main/java/uz/nt/uzumproject/repository/ProductRepository.java
+++ src/main/java/uz/nt/uzumproject/repository/ProductRepository.java
@@ -1,8 +1,11 @@
 package uz.nt.uzumproject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import uz.nt.uzumproject.model.Product;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
-
+//    @Query(name = "select * from product t where (t.category_id, t.price) in ")
 }
