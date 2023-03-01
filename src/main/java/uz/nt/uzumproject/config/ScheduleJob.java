6709135bc7a37a15498d21a883a00383d3c1ce6b
Saidafzalkhon <Saidafzalkhon.developer@gmail.com>
2023-03-01 14:01:40 +0500

    Add exel saved

--- src/main/java/uz/nt/uzumproject/config/ScheduleJob.java
+++ src/main/java/uz/nt/uzumproject/config/ScheduleJob.java
@@ -2,6 +2,9 @@ package uz.nt.uzumproject.config;
 
 import com.google.gson.Gson;
 import jakarta.transaction.Transactional;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.Async;
@@ -13,10 +16,7 @@ import uz.nt.uzumproject.model.Product;
 import uz.nt.uzumproject.repository.ProductRepository;
 import uz.nt.uzumproject.service.ImageService;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -43,7 +43,7 @@ public class ScheduleJob {
     @Scheduled(fixedDelay = 1, timeUnit = TimeUnit.MINUTES)
     public void report() throws IOException {
         List<Product> products = productRepository.findAllByAmountLessThanEqual(50);
-        File f = new File(ImageService.filePath("report", "csv"));
+        File f = new File(ImageService.filePath("report", ".csv"));
         if (f.createNewFile()){
             FileOutputStream fos = new FileOutputStream(f);
             PrintWriter pw = new PrintWriter(fos);
@@ -57,5 +57,39 @@ public class ScheduleJob {
             pw.close();
             fos.close();
         }
+    }
+    @Scheduled(fixedDelay = 2, timeUnit = TimeUnit.SECONDS)
+    public void toExel() throws IOException {
+        List<Product> products = productRepository.findAllByAmountLessThanEqual(50);
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("Java");
+        HSSFRow row = sheet.createRow(0);
+        row.createCell(0).setCellValue("ID");
+        row.createCell(1).setCellValue("Name");
+        row.createCell(2).setCellValue("Amount");
+        row.createCell(3).setCellValue("Description");
+        row.createCell(4).setCellValue("Is-Available");
+        row.createCell(5).setCellValue("price");
+        row.createCell(6).setCellValue("category_id");
+        int index = 0;
+        for (Product product : products) {
+            HSSFRow row1 = sheet.createRow(index);
+            row1.createCell(0).setCellValue(product.getId());
+            row1.createCell(1).setCellValue(product.getName());
+            row1.createCell(2).setCellValue(product.getAmount());
+            row1.createCell(3).setCellValue(product.getDescription());
+            row1.createCell(4).setCellValue(product.getIsAvailable());
+            row1.createCell(5).setCellValue(product.getPrice());
+            row1.createCell(6).setCellValue(product.getCategory().getId());
+            index++;
+        }
+
+        File f = new File(ImageService.filePath("Product", ".xls"));
+        FileOutputStream fos = new FileOutputStream(f);
+        workbook.write(fos);
+        workbook.close();
+        fos.close();
+
+
     }
 }
